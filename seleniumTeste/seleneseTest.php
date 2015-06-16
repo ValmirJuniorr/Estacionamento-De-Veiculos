e9c3a62a6c68443aafff26c97349d38bc1d115b9
Valmir <Valmir@Valmir-PC>
2015-06-16 18:43:10 -0300

    ultimo comiit

--- seleniumTeste/seleneseTest.php
+++ seleniumTeste/seleneseTest.php
@@ -17,7 +17,7 @@ require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
 class seleneseTest extends PHPUnit_Extensions_SeleniumTestCase {
 
     function setUp() {
-        $this->setBrowser("*chrome");
+        $this->setBrowser("*firefox");
         $this->setBrowserUrl("http://localhost/ParkingManagerment");
     }
 
